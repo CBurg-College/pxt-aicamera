8317ef73a352be0edbb6c96f2979ca1e2fc89341
CBurg-College <cburgmicro@outlook.com>
2025-07-31 21:59:47 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -62,12 +62,51 @@ namespace CameraAI {
         return DataBuffer[4]
     }
 
-    export function itemColor(): number {
-        return DataBuffer[1]
+    export function itemColor(): Color {
+        // measured color numbers:
+        // =======================
+        // 0 = red
+        // 1 = green
+        // 2 = blue
+        // 3 = yellow
+        // 4 = black
+        // 5 = orange
+        // 6 = white
+
+        switch (DataBuffer[1]) {
+            case 0: return Color.Red;
+            case 1: return Color.Green;
+            case 2: return Color.Blue;
+            case 3: return Color.Yellow;
+            case 4: return Color.Black;
+            case 5: return Color.Orange;
+            case 6: return Color.White;
+        }
+        return Color.None
     }
 
-    export function itemIsColor(col: Color): boolean {
-        return (DataBuffer[1] == col)
+    export function itemIsColor(color: Color): boolean {
+        // measured color numbers:
+        // =======================
+        // red = 0
+        // green = 1
+        // blue = 2
+        // yellow = 3
+        // black = 4
+        // orange = 5
+        // white = 6
+
+        switch (color) {
+            case Color.Red: return (DataBuffer[1] == 0)
+            case Color.Green: return (DataBuffer[1] == 1)
+            case Color.Blue: return (DataBuffer[1] == 2)
+            case Color.Yellow: return (DataBuffer[1] == 3)
+            case Color.Black: return (DataBuffer[1] == 4)
+            case Color.Orange: return (DataBuffer[1] == 5)
+            case Color.White: return (DataBuffer[1] == 6)
+        }
+
+        return false
     }
 
     export function itemID(): number {
